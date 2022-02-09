9e957774868ba23c6d0ae9a1ae636b209b48c91e
Iuliia El Sammak <juliag875@gmail.com>
2022-02-09 16:39:34 -0500

    commit tree bf4b33f64fbd035c97b0a9748b5d38f025785bc3

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,47 @@
+import {useState} from 'react'
 import video from "../data/video.js";
+import CommentsContainer from './CommentsContainer.jsx';
+import MainContent from './MainContent.jsx';
+import VideoDisplay from "./VideoDisplay.jsx";
 
 function App() {
-  console.log("Here's your data:", video);
+  const [videoObj, setVideoObj] = useState(video)
+  const [toggleComments, setToggleComments] = useState(true)
+
+
+  function addVote(){
+    // const newObj = {...videoObj, upvotes: videoObj.upvotes + 1}
+    // setVideoObj(newObj)
+
+    setVideoObj(currentState => {
+      return {...currentState, upvotes: currentState.upvotes + 1}
+    })
+  }
+  
+  function subtractVote(){
+    // const newObj = {...videoObj, downvotes: videoObj.downvotes - 1}
+    // setVideoObj(newObj)
+  
+    setVideoObj(currentState => {
+      return {...currentState, downvotes: currentState.downvotes - 1}
+    })
+  }
+  
+  function handleCommentToggle() {
+    setToggleComments(currentToggleState => !currentToggleState)
+  }
 
   return (
     <div className="App">
-      <iframe
-        width="919"
-        height="525"
-        src="https://www.youtube.com/embed/dQw4w9WgXcQ"
-        frameBorder="0"
-        allowFullScreen
-        title="Thinking in React"
-      />
+      <VideoDisplay embedUrl={videoObj.embedUrl}/>
+
+      <MainContent addVote={addVote} subtractVote={subtractVote} 
+      toggleComments={toggleComments} 
+      handleCommentToggle={handleCommentToggle} 
+      title={videoObj.title} createdAt={videoObj.createdAt} views={videoObj.views} 
+      upvotes={videoObj.upvotes} downvotes={videoObj.downvotes}/>
+      
+      {toggleComments && <CommentsContainer comments={videoObj.comments}/> }
     </div>
   );
 }
---
+++ src/components/Comment.jsx
@@ -0,0 +1,12 @@
+
+function Comment({comment, user}) {
+  return (
+    <div>
+      <h4>{user}</h4>
+      <p>{comment}</p>
+    </div>
+  )
+}
+
+
+export default Comment;
